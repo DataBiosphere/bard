16358df2dde8b673c1594f804b29894dc66d9e78
Pete Santos <psantos@broadinstitute.org>
2020-02-06 12:58:46 -0500

    Initial code check in

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Bard 
 Metrics Collection Service
+
+## Overview
+This service will accept requests from a client application for the purposes of logging user actions and forwarding
+them to to an analytics service so we can determine how our users utilize our applications
+
+## Typical Usage
+1. Call ```event``` to log a user's action and send the event to mixpanel.
+
+      Only event on the production system will be sent to mixpanel
+
+## Authentication
+1. The service will only accept requests from users who are registered with our application
+
+## Developing
+Note that there is currently no separate development environment, so any changes will affect the real system (e.g. if you enable the mixpanel API with a token, events will get pushed to mixpanel). Use caution.
+
+1. Download a key for the app engine default service account terra-metrics-dev@appspot.gserviceaccount.com and note the location
+2. Install the dependencies
+   
+    ```sh
+    yarn install
+    ```
+
+3. Start a dev server on port 8080 with auto-reload
+
+    ```sh
+    GCP_PROJECT=terra-dev-metrics GOOGLE_APPLICATION_CREDENTIALS=<path-to-key-file> yarn run start-dev
+    ```
+
+4. Lint any code changes using 
+    ```sh
+    yarn run lint
+    ```
+   
+## Documentation
+  To generate the [API docs](https://terra-metrics-dev.appspot.com/docs) run
+
+  ```sh 
+    yarn run generate-docs
+  ```
+   
---
+++ src/index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const { promiseHandler, Response } = require('./utils')
+const { project, samRoot, mixPanelRoot } = require('../config')
+const { logger, getSecret } = require('./google-utils')
+const btoa = require('btoa-lite')
+const fetch = require('node-fetch')
+
+
+const withAuth = wrappedFn => async (req, ...args) => {
+  const authRes = await fetch(
+    `${samRoot}/register/user/v2/self/info`,
+    { headers: { authorization: req.headers.authorization } }
+  ).catch(() => {
+    throw new Response(503, 'Unable to contact auth service')
+  })
+
+  if (authRes.status === 401) {
+    throw new Response(401, 'Unauthorized')
+  }
+
+  if (!authRes.ok) {
+    console.error('Sam error', await authRes.text())
+    throw new Response(503, 'Failed to query auth service')
+  }
+
+  const { enabled } = await authRes.json()
+  if (!enabled) {
+    throw new Response(403, 'Forbidden')
+  }
+  return wrappedFn(req, ...args)
+}
+
+const sendToMixpanel = async (token, event, props) => {
+  const data = btoa(JSON.stringify({ event, properties: { token, ...props } }))
+  try {
+    await fetch(`${mixPanelRoot}/track?data=${data}`)
+  } catch (error) {
+    console.log(`Failed to log ${data} to mixpanel`)
+  }
+}
+
+const main = async () => {
+  const token = await getSecret({ project, secretName: 'mixpanel-test-api' })
+  const log = logger({ project, logName: 'metrics' })
+
+  const app = express()
+  app.use(bodyParser.json())
+  app.use(cors())
+  app.use('/docs', express.static('docs'))
+
+  app.listen(process.env.PORT || 8080)
+
+  /**
+   * @api {get} /status System status
+   * @apiName status
+   * @apiVersion 1.0.0
+   * @apiGroup System
+   * @apiSuccess (Success 200) -
+   */
+  app.get('/status', promiseHandler(async () => {
+    return new Response(200)
+  }))
+
+  /**
+   * @api {post} /event System status
+   * @apiDescription Records the event to a log and forwards it to mixpanel
+   * @apiName event
+   * @apiVersion 1.0.0
+   * @apiGroup Events
+   * @apiParam {String} event Name of the event
+   * @apiParam {String} properties Properties associated with this event
+   * @apiSuccess (Success 200) -
+   */
+  app.post('/api/event', promiseHandler(withAuth(async req => {
+    const { event, properties } = req.body
+
+    log(req.body)
+    token && sendToMixpanel(token, event, properties)
+
+    return new Response(200, { event, properties })
+  })))
+}
+
+main().catch(console.error)
+
