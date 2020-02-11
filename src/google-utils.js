76b4a2c0d6de76db5a71b29d6be13023c6608d7e
Pete Santos <50371603+petesantos@users.noreply.github.com>
2020-02-11 15:33:50 -0500

    Initial check in of Bard service (#1)
    * Initial code check in
    
    * move some files
    
    * Readme updates
    
    * Remove index.yaml
    
    * Fixed package.json main file location
    
    * pin yarn version
    
    * Update with feedback

---
+++ src/google-utils.js
@@ -0,0 +1,35 @@
+const { Logging } = require('@google-cloud/logging')
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
+
+const logger = ({ project, logName, type = 'global' }) => {
+  const logging = new Logging({ projectId: project })
+  const log = logging.log(logName)
+  const metadata = {
+    resource: { type }
+  }
+
+  return async message => {
+    try {
+      await log.write(log.entry(metadata, message))
+    } catch (error) {
+      console.error(`Could not write ${JSON.stringify(message)} to ${logName} in stackdriver: ${JSON.stringify(error)}`)
+    }
+  }
+}
+
+const getSecret = async ({ project, secretName }) =>  {
+  const client = new SecretManagerServiceClient()
+  const name = `projects/${project}/secrets/${secretName}/versions/latest`
+
+  try {
+    const [version] = await client.accessSecretVersion({ name })
+    return version.payload.data.toString('utf8')
+  } catch (error) {
+    return null
+  }
+}
+
+module.exports = {
+  logger,
+  getSecret
+}
