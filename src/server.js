1f2d4ceed9cf87dc563992cdc2ab9bd7a4d10cbc
Pete Santos <psantos@broadinstitute.org>
2020-02-26 10:05:58 -0500

    Add configs for all envs

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const { promiseHandler, Response } = require('./utils')
-const { project, samRoot, mixPanelRoot } = require('../config')
+const { project, samRoot } = require('../config')
 const { logger, getSecret } = require('./google-utils')
 const btoa = require('btoa-lite')
 const fetch = require('node-fetch')
@@ -35,7 +35,7 @@ const withAuth = wrappedFn => async (req, ...args) => {
 const sendToMixpanel = async (token, event, props) => {
   const data = btoa(JSON.stringify({ event, properties: { token, ...props } }))
   try {
-    await fetch(`${mixPanelRoot}/track?data=${data}`)
+    await fetch(`https://api.mixpanel.com/track?data=${data}`)
   } catch (error) {
     console.error(`Failed to log ${data} to mixpanel`)
   }
