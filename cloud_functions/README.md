6a7c065e693804c7ebaabd7eff915cbef694e688
Brian Reilly <breilly@broadinstitute.org>
2020-12-04 10:24:40 -0500

    Add cloud function to flag cryptominers in Mixpanel

---
+++ cloud_functions/README.md
@@ -0,0 +1,15 @@
+# Bard - Cloud Functions
+Cloud functions related to metrics
+
+## Overview
+This is a collection of cloud functions that augment the core bard service. The main service is a set of endpoints intended to be called from clients, either anonymously or with an access token that identifies the user performing the action. The cloud functions here may have other triggers, such as a Google Pub/Sub message.
+
+## Function Documentation
+
+### `flagCryptominer`
+`flagCryptominer` watches for messages on the `terra-bard-[env]/topics/cryptominers` Pub/Sub topic containing IDs of identified cryptominer accounts and sets the "Is Cryptominer" flag in their Mixpanel profile to `true`.
+
+## Deployment
+```sh
+gcloud --project terra-bard-[env] functions deploy flagCryptominer --runtime nodejs12 --trigger-topic cryptominers
+```
---
+++ cloud_functions/function.js
@@ -0,0 +1,67 @@
+const { project } = require('./config')
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
+const fetch = require('node-fetch')
+
+
+const flagCryptominer = async (message, context) => {
+  const subjectId = Buffer.from(message.data, 'base64').toString();
+  const token = await getSecret({ project, secretName: 'mixpanel-api' })
+  const data = {
+    $token: token,
+    $distinct_id: `google:${subjectId}`,
+    $set: {
+      'Is Cryptominer': true
+    }
+  }
+  return fetchMixpanelV2('engage#profile-set', { data })
+}
+
+// TODO: Share this code with the bard GAE app (will probably require a more sophisticated build/deploy process)
+
+const getSecret = async ({ project, secretName }) =>  {
+  const client = new SecretManagerServiceClient()
+  const name = `projects/${project}/secrets/${secretName}/versions/latest`
+
+  try {
+    const [version] = await client.accessSecretVersion({ name })
+    return version.payload.data.toString('utf8')
+  } catch (error) {
+    return null
+  }
+}
+
+class Response {
+  constructor(status, data) {
+    this.status = status
+    this.data = data
+  }
+}
+
+const fetchOk = async (url, { serviceName, ...options } = {}) => {
+  const res = await fetch(url, options).catch(() => {
+    throw new Response(503, `Unable to contact ${serviceName} service`)
+  })
+  if (res.status === 401) {
+    throw new Response(401, 'Unauthorized')
+  }
+  if (!res.ok) {
+    console.error(`${serviceName} error`, await res.text())
+    throw new Response(503, `Failed to query ${serviceName} service`)
+  }
+  return res
+}
+
+const fetchMixpanelV2 = async (url, { data, ...options } = {}) => {
+  const body = new URLSearchParams({ data: JSON.stringify(data) })
+  const res = await fetchOk(`https://api.mixpanel.com/${url}`,
+      { serviceName: 'metrics', method: 'POST', body, ...options })
+  const status = await res.text()
+  if (status !== '1') {
+    console.error(`Failed to log to mixpanel:`, status, data)
+    throw new Response(400, 'Error saving metrics data')
+  }
+}
+
+module.exports = {
+  flagCryptominer
+}
