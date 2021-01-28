f617b6773f2273500e8b5ffaf8cd92a4da6efece
Brian Reilly <breilly@broadinstitute.org>
2021-01-28 15:11:19 -0500

    HTTP-trigger cloud function [BT-51] (#27)

--- cloud_functions/flagCryptominer.js
+++ cloud_functions/flagCryptominer.js
@@ -4,11 +4,20 @@ const utils = require('./utils')
 
 const run = utils.withErrorReporting((mixpanel, message) => {
   const userSubjectId = validate(message)
-  return promisify(mixpanel.people.set)(`google:${userSubjectId}`, { 'isCryptominer': true })
+  return new Promise((resolve, reject) => {
+    mixpanel.people.set(`google:${userSubjectId}`, { 'isCryptominer': true },
+        error => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve()
+          }
+        })
+  })
 })
 
 const validate = message => {
-  if (!(message.attributes && message.attributes.cryptominer === true)) {
+  if (!(message.attributes && (message.attributes.cryptominer === true || message.attributes.cryptominer === 'true'))) {
     throw new Error('Unexpected message; missing `cryptominer: true` attribute. Is the Pub/Sub subscription set up correctly?')
   }
   const data = message.data && JSON.parse(Buffer.from(message.data, 'base64').toString())
