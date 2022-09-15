3f82a135c7b9ecde2ab0bb5be095dfb031770958
Gabe de Mattos <gdematto@broadinstitute.org>
2022-09-15 17:13:24 -0400

    chore: some more refactor in favor of lodash and functional programming

--- src/account-type-utils.js
+++ src/account-type-utils.js
@@ -7,59 +7,49 @@ const _ = require('lodash')
  *
  */
 
-const userTypes = {
-  BroadEmployee: 'Broad Employee',
-  VerilyEmployee: 'Verily Employee',
-  IndependentUser: 'Independent User',
-  EnterpriseUser: 'Enterprise User',
-  ServiceAccountUser: 'Service Account User',
-  EducationalInstituteUser: 'Educational Institute User',
-  NonProfitUser: 'Non-profit User',
-  GovUser: 'Government User',
-  Other: 'Other'
-}
-
 const generalTld = [
-  'co', 'app', 'us', 'uk', 'ca',
   'de', 'fr', 'jp', 'au', 'ru', 'ch', 'se', 'no', 'nl', 'it',
   'es', 'dk', 'cz', 'br', 'be', 'at', 'ar', 'in', 'mx', 'pl',
   'pt', 'fi', 'gr', 'hk', 'id', 'ie', 'il', 'is', 'kr', 'my',
   'nz', 'ph', 'sg', 'th', 'tw', 'vn', 'cn'
 ]
 
-const domainTlds = {
-  '.broadinstitute.org': userTypes.BroadEmployee,
-  '.firecloud.org': userTypes.BroadEmployee,
-  '.verily.com': userTypes.VerilyEmployee,
-  '.google.com': userTypes.VerilyEmployee,
-  '.gmail.com': userTypes.IndependentUser,
-  '.gserviceaccount.com': userTypes.ServiceAccountUser,
-  '.com': userTypes.EnterpriseUser,
-  '.net': userTypes.EnterpriseUser,
-  '.co': userTypes.EnterpriseUser,
-  '.io': userTypes.EnterpriseUser,
-  '.ca': userTypes.EnterpriseUser,
-  '.ai': userTypes.EnterpriseUser,
-  '.edu': userTypes.EducationalInstituteUser,
-  '.org': userTypes.NonProfitUser,
-  '.gov': userTypes.GovUser
+const accountTypeMapping = {
+  'Broad Employee': ['.broadinstitute.org', '.firecloud.org'],
+  'Verily Employee': ['.verily.com', '.verilylifesciences.com', '.google.com'],
+  'Service Account User': ['.gserviceaccount.com'],
+  'Independent User': ['.gmail.com'],
+  'Educational Institute User': ['.edu'],
+  'Non-profit User': ['.org'],
+  'Government User': ['.gov'],
+  'Enterprise User': ['.com', '.net', '.io', '.co', '.app', '.us', 'uk', 'ca']
 }
+
 
 const getAccountType = email => {
   if (!validator.isEmail(email)) {
-    return userTypes.Other
+    return 'Unknown'
   }
 
   const domain = '.' + email.split('@').pop()
   const lastTwoDomainParts = '.' + _.join(_.takeRight(domain.split('.'), 2), '.')
 
-  const accountType = _.map(domainTlds, (value, key) => {
-    if (domain.endsWith(key) || _.some(generalTld, tld => lastTwoDomainParts.endsWith(`${key}.${tld}`))) {
-      return value
+  const accountType = _.map(accountTypeMapping, (domainList, userType) => {
+    if (_.some(domainList, domainItem => _.endsWith(domain, domainItem))) {
+      return userType
     }
+
+    const matchingTld = _.some(domainList, domainObj => {
+      return _.some(generalTld, tld => {
+        return lastTwoDomainParts === `${domainObj}.${tld}` ||
+               lastTwoDomainParts === `${domainObj}.${tld}.`
+      })
+    })
+
+    return _.size(domainList) === 1 && matchingTld && userType
   })
 
-  return _.find(accountType) || userTypes.Other
+  return _.find(accountType) || 'Unknown'
 }
 
 module.exports = { getAccountType }
