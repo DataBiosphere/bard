dd6d2891971d30b49daa3074ace8a6ae6f8cf55c
nmalfroy <nmalfroy@broadinstitute.org>
2023-08-30 14:40:28 -0400

    Refactor server.js into components for testing

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// Config loading is separate so it can be mocked in tests
+const { project, orchestrationRoot, samRoot } = require('../config')
+
+module.exports = { project, orchestrationRoot, samRoot }
---
+++ src/sam-utils.js
@@ -0,0 +1,18 @@
+const { samRoot} = require('./config')
+const { fetchOk, Response } = require('./utils')
+
+// Verify the user's auth token with Sam
+const verifyAuth = async req => {
+  const res = await fetchOk(
+    `${samRoot}/register/user/v2/self/info`,
+    { headers: { authorization: req.headers.authorization }, serviceName: 'auth' }
+  )
+  req.user = await res.json()
+  if (!req.user.enabled) {
+    throw new Response(403, 'Forbidden')
+  }
+}
+
+module.exports = {
+  verifyAuth
+}
