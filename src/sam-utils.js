d8bedd1208e88e7f03d3155d662df351f733ad1d
Tristan Garwood <tristan.garwood@gmail.com>
2024-05-13 11:27:37 -0400

    change cache settings

--- src/sam-utils.js
+++ src/sam-utils.js
@@ -3,7 +3,20 @@ const { fetchOk, Response } = require('./utils')
 const { jwtDecode }   = require('jwt-decode')
 const NodeCache = require('node-cache')
 
-const samUserCache = new NodeCache({ stdTTL: 300, checkperiod: 120, maxKeys: 1000 })
+const samUserCache = new NodeCache({ stdTTL: 300, checkperiod: 300, maxKeys: 10000 })
+
+async function getSamUser(req, userEmail) {
+  const res = await fetchOk(
+    `${samRoot}/register/user/v2/self/info`,
+    { headers: { authorization: req.headers.authorization }, serviceName: 'auth' }
+  )
+  const samUser = await res.json()
+  samUserCache.set(userEmail, samUser)
+  req.user = samUser
+  if (!req.user.enabled) {
+    throw new Response(403, 'Forbidden')
+  }
+}
 
 // Verify the user's auth token with Sam
 const verifyAuth = async req => {
@@ -12,16 +25,7 @@ const verifyAuth = async req => {
   const maybeCachedUser = samUserCache.get(userEmail)
 
   if (maybeCachedUser === undefined) {
-    const res = await fetchOk(
-      `${samRoot}/register/user/v2/self/info`,
-      { headers: { authorization: req.headers.authorization }, serviceName: 'auth' }
-    )
-    const samUser = await res.json()
-    samUserCache.set(userEmail, samUser)
-    req.user = samUser
-    if (!req.user.enabled) {
-      throw new Response(403, 'Forbidden')
-    }
+    await getSamUser(req, userEmail)
   } else {
     req.user = maybeCachedUser
   }
