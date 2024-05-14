3652bba85b793f5e31fd1ae4a8130fa67ea26f32
Tristan Garwood <tristan.garwood@gmail.com>
2024-05-14 10:39:09 -0400

    ID-1277 Add cache and event labeling (#74)
    * ID-1277 Add sam user cache, upgrade node
    
    * change cache settings
    
    * Update node version in circle config
    
    * fix tests
    
    * version bump
    
    * version bump

--- src/sam-utils.js
+++ src/sam-utils.js
@@ -1,15 +1,33 @@
 const { samRoot } = require('./config')
 const { fetchOk, Response } = require('./utils')
+const { jwtDecode }   = require('jwt-decode')
+const NodeCache = require('node-cache')
 
-// Verify the user's auth token with Sam
-const verifyAuth = async req => {
+const samUserCache = new NodeCache({ stdTTL: 300, checkperiod: 300, maxKeys: 10000 })
+
+async function getSamUser(req, userEmail) {
   const res = await fetchOk(
     `${samRoot}/register/user/v2/self/info`,
     { headers: { authorization: req.headers.authorization }, serviceName: 'auth' }
   )
-  req.user = await res.json()
+  const samUser = await res.json()
+  samUserCache.set(userEmail, samUser)
+  req.user = samUser
   if (!req.user.enabled) {
     throw new Response(403, 'Forbidden')
+  }
+}
+
+// Verify the user's auth token with Sam
+const verifyAuth = async req => {
+  const decodedToken = jwtDecode(req.headers.authorization.replace('Bearer ', ''))
+  const userEmail = decodedToken.email
+  const maybeCachedUser = samUserCache.get(userEmail)
+
+  if (maybeCachedUser === undefined) {
+    await getSamUser(req, userEmail)
+  } else {
+    req.user = maybeCachedUser
   }
 }
 
